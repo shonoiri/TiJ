6aceb3565a2dd449a99f6510004097c87f4a3755
Irina Shonova <irina-shonova@ya.ru>
2015-08-07 16:38:47 +0400

    source folder added

---
+++ source/typeinfo/pets/Integer.java
@@ -0,0 +1,7 @@
+//: typeinfo/pets/Cymric.java
+package typeinfo.pets;
+
+public class Integer extends Manx {
+  public Integer(String name) { super(name); }
+  public Integer() { super(); }
+} ///:~
