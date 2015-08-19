7acd98fbc86f601603a5b3d17d9ec8264331be41
Irina Shonova <irina-shonova@ya.ru>
2015-08-19 15:47:14 +0400

    Package identifier added

--- source/generics/TupleTest2.java
+++ source/generics/TupleTest2.java
@@ -1,32 +1,43 @@
+package generics;
+
 //: generics/TupleTest2.java
 import net.mindview.util.*;
 import static net.mindview.util.Tuple.*;
+import ex3.SixTuple;
 
 public class TupleTest2 {
-  static TwoTuple<String,Integer> f() {
-    return tuple("hi", 47);
-  }
-  static TwoTuple f2() { return tuple("hi", 47); }
-  static ThreeTuple<Amphibian,String,Integer> g() {
-    return tuple(new Amphibian(), "hi", 47);
-  }
-  static
-  FourTuple<Vehicle,Amphibian,String,Integer> h() {
-    return tuple(new Vehicle(), new Amphibian(), "hi", 47);
-  }
-  static
-  FiveTuple<Vehicle,Amphibian,String,Integer,Double> k() {
-    return tuple(new Vehicle(), new Amphibian(),
-      "hi", 47, 11.1);
-  }
-  public static void main(String[] args) {
-    TwoTuple<String,Integer> ttsi = f();
-    System.out.println(ttsi);
-    System.out.println(f2());
-    System.out.println(g());
-    System.out.println(h());
-    System.out.println(k());
-  }
+	static TwoTuple<String, Integer> f() {
+		return tuple("hi", 47);
+	}
+
+	static TwoTuple f2() {
+		return tuple("hi", 47);
+	}
+
+	static ThreeTuple<Amphibian, String, Integer> g() {
+		return tuple(new Amphibian(), "hi", 47);
+	}
+
+	static FourTuple<Vehicle, Amphibian, String, Integer> h() {
+		return tuple(new Vehicle(), new Amphibian(), "hi", 47);
+	}
+
+	static FiveTuple<Vehicle, Amphibian, String, Integer, Double> k() {
+		return tuple(new Vehicle(), new Amphibian(), "hi", 47, 11.1);
+	}
+	static SixTuple<Vehicle, Amphibian, String, Integer, Double, Float> l(){
+		return tuple(new Vehicle(), new Amphibian(), "hi", 47, 11.1, 12.0F);
+	}
+
+	public static void main(String[] args) {
+		TwoTuple<String, Integer> ttsi = f();
+		System.out.println(ttsi);
+		System.out.println(f2());
+		System.out.println(g());
+		System.out.println(h());
+		System.out.println(k());
+		System.out.println(l());
+	}
 } /* Output: (80% match)
 (hi, 47)
 (hi, 47)
