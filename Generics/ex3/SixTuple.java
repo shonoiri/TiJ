e9325c7ca30b648c7e4289be231b19e8802f164e
Irina Shonova <irina-shonova@ya.ru>
2015-08-19 15:48:20 +0400

    toString method changed

--- Generics/ex3/SixTuple.java
+++ Generics/ex3/SixTuple.java
@@ -3,6 +3,11 @@ package ex3;
 import net.mindview.util.FiveTuple;
 
 public class SixTuple <A, B, C, D, E, F> extends FiveTuple<A, B, C, D, E> {
+	@Override
+	public String toString() {
+		return "(" + first + ", " + second + ", " + third + ", " + fourth + ", " + fifth + ", " + f + ")";
+	}
+
 	public final F f;
 
 	public SixTuple(A a, B b, C c, D d, E e, F f) {
